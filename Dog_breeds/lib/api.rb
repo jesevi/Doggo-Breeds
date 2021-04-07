05ba70bd2f5d5a79de393ac164f00d724ecec5fe
Jason Sevilla <76412035+jesevi@users.noreply.github.com>
2021-04-07 13:15:56 -0400

    Add files via upload

---
+++ Dog_breeds/lib/api.rb
@@ -0,0 +1,21 @@
+class API
+
+    def initialize
+        @url = "https://api.thedogapi.com/v1/breeds"
+    end
+
+    def get_breed_data
+        breed_hash = HTTParty.get(@url)
+        breed_array = breed_hash
+        self.create_breed_objects(breed_array)
+    end
+
+    def create_breed_objects(breed_array)
+        breed_array.each do |breed_hash|
+            DogBreed.new(breed_hash)
+        end
+    end
+
+
+
+end
